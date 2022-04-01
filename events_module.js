098f7c5f864990f9710b9926da83d0555b4db32b
Rabindra <drarabin@gmail.com>
2022-04-01 21:43:56 +0545

    extending EventEmitter

--- events_module.js
+++ events_module.js
@@ -3,10 +3,12 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function (arg) {
+emitter.on('messageLogged', (arg) => { // arrow function
     console.log('Listener called', arg);
 });
 
 // Raise an event
 emitter.emit('messageLogged', { id: 1, url: 'http://' });
+
+
 
