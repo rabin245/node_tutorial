2159133f2887ca199b23d5469dccde37d05e553b
Rabindra <drarabin@gmail.com>
2022-04-01 21:29:15 +0545

    events module (add and raise event)

---
+++ events_module.js
@@ -0,0 +1,12 @@
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on('messageLogged', function (arg) {
+    console.log('Listener called', arg);
+});
+
+// Raise an event
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
+
