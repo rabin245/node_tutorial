c870e7b930ab518c59490948ae257b6dd8e6a5b1
Rabindra <drarabin@gmail.com>
2022-04-02 14:32:49 +0545

    handling http post request

--- REST API/express-demo/index.js
+++ REST API/express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -15,6 +17,14 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name,
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
@@ -26,6 +36,8 @@ app.get('/api/posts/:year/:month', (req, res) => {
     // res.send(req.params);
     res.send(req.query);
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
