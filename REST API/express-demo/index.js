516e6ddd912c62abb3d186b903b7ad8d651aa315
Rabindra <drarabin@gmail.com>
2022-04-02 14:20:04 +0545

    route parameters

--- REST API/express-demo/index.js
+++ REST API/express-demo/index.js
@@ -9,4 +9,15 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    // res.send(req.params);
+    res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
