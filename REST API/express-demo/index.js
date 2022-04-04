a50ddde452302433cf2f89029c70a03dc62fb79b
Rabindra <drarabin@gmail.com>
2022-04-04 08:40:12 +0545

    input validation

--- REST API/express-demo/index.js
+++ REST API/express-demo/index.js
@@ -18,6 +18,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 Bad Request
+        res.status(400).send('Name is required and should be min 3 characters.');
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name,
