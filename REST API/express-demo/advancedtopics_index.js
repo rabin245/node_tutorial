59d29b1c17ef11de23a62e23276ce54f40d43401
rabin245 <drarabin@gmail.com>
2022-05-06 22:54:02 +0545

    some builtin middlewares

--- REST API/express-demo/advancedtopics_index.js
+++ REST API/express-demo/advancedtopics_index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // custom middleware
 app.use(log);
