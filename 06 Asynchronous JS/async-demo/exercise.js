800e93b6bfc2c02104cf8fb652dcb18f197a0fac
rabin245 <drarabin@gmail.com>
2022-05-11 00:12:54 +0545

    exercise completed

--- 06 Asynchronous JS/async-demo/exercise.js
+++ 06 Asynchronous JS/async-demo/exercise.js
@@ -1,34 +1,52 @@
-getCustomer(1, (customer) => {
+// getCustomer(1, (customer) => {
+//   console.log("Customer: ", customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log("Top movies: ", movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log("Email sent...");
+//       });
+//     });
+//   }
+// });
+
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
   console.log("Customer: ", customer);
   if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log("Top movies: ", movies);
-      sendEmail(customer.email, movies, () => {
-        console.log("Email sent...");
-      });
-    });
+    const topMovies = await getTopMovies();
+    console.log("Top movies: ", topMovies);
+    await sendEmail(customer.email, topMovies);
+    console.log("Email sent...");
   }
-});
+}
+notifyCustomer();
 
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({
-      id: 1,
-      name: "Mosh Hamedani",
-      isGold: true,
-      email: "email",
-    });
-  }, 4000);
+function getCustomer(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: "Mosh Hamedani",
+        isGold: true,
+        email: "email",
+      });
+    }, 4000);
+  });
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(["movie1", "movie2"]);
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 4000);
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
 }
