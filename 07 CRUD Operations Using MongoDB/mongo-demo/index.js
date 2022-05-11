aa896d01c4168fbe3bbcf70d57d16d3016c5e1d3
rabin245 <drarabin@gmail.com>
2022-05-11 19:49:35 +0545

    schemas

--- 07 CRUD Operations Using MongoDB/mongo-demo/index.js
+++ 07 CRUD Operations Using MongoDB/mongo-demo/index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
