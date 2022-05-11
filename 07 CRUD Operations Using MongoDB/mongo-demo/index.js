e950a4942dfb32bc9860f9a7a4af624380e97efb
rabin245 <drarabin@gmail.com>
2022-05-11 19:53:20 +0545

    saving a document

--- 07 CRUD Operations Using MongoDB/mongo-demo/index.js
+++ 07 CRUD Operations Using MongoDB/mongo-demo/index.js
@@ -13,10 +13,17 @@ const courseSchema = new mongoose.Schema({
   isPublished: Boolean,
 });
 
-const Course = mongoose.model("Course", courseSchema);
-const course = new Course({
-  name: "Node.js Course",
-  author: "Late",
-  tags: ["node", "backend"],
-  isPublished: true,
-});
+async function createCourse() {
+  const Course = mongoose.model("Course", courseSchema);
+  const course = new Course({
+    name: "Node.js Course",
+    author: "Late",
+    tags: ["node", "backend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
