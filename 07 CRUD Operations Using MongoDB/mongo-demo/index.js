be3feafe771ace0fd2e3f391fcd55db3ac2eb6f8
rabin245 <drarabin@gmail.com>
2022-05-11 22:43:37 +0545

    logical query operators

--- 07 CRUD Operations Using MongoDB/mongo-demo/index.js
+++ 07 CRUD Operations Using MongoDB/mongo-demo/index.js
@@ -27,6 +27,7 @@ async function createCourse() {
 }
 
 async function getCourse() {
+  // Comparison query operators
   // eq (equal)
   // ne (not equal)
   // gt (greater than)
@@ -36,11 +37,19 @@ async function getCourse() {
   // in
   // nin (not in)
 
+  // Logical query operators
+  // or
+  // and
+
   const courses = await Course
-    //   .find({ author: "Late", isPublished: true,})
-    .find({ price: { $gt: 10 } }) // greater than 10
-    .find({ price: { $gte: 10, $lte: 20 } }) // greater-equal to 10 and less-eqaul to 20
-    .find({ price: { $in: [10, 15, 20] } }) // price equal to 10 or 15 or 20
+    // .find({ author: "Late", isPublished: true })
+    // comparison operators
+    // .find({ price: { $gt: 10 } }) // greater than 10
+    // .find({ price: { $gte: 10, $lte: 20 } }) // greater-equal to 10 and less-eqaul to 20
+    // .find({ price: { $in: [10, 15, 20] } }) // price equal to 10 or 15 or 20
+    // logical operators
+    .find()
+    .or([{ author: "Late" }, { isPublished: true }])
     .limit(10)
     .sort({ name: 1 }) // 1=>ascending order, -1=>descending order
     .select({ name: 1, tags: 1 });
