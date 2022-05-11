c25d9a2d87965880b63879b35ecca0f2b8ec8210
rabin245 <drarabin@gmail.com>
2022-05-11 22:51:29 +0545

    counting

---
+++ 07 CRUD Operations Using MongoDB/mongo-demo/index_2.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Late",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  const courses = await Course.find({ author: "Late", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .count();
+  console.log(courses); // returns the count of mathcing documents
+}
+
+// createCourse();
+getCourses();
