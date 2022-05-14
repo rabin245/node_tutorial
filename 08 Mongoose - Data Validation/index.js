f6b8c3e21ec917763d80f3414b8ea8b6b4b41864
rabin245 <drarabin@gmail.com>
2022-05-14 12:43:37 +0545

    validation intro

---
+++ 08 Mongoose - Data Validation/index.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to db successfully..."))
+  .catch((err) => console.error("Connection to db failed...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    // name: "Test Course",
+    author: "Late",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+    price: 69,
+  });
+
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
+}
+
+async function getCourses() {
+  const pageNumber = 1;
+  const pageSize = 10;
+
+  const courses = await Course.find({ author: "Late", isPublished: true })
+    .limit(pageSize)
+    .skip((pageNumber - 1) * pageSize)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+async function updateCourseQueryFirst(id) {
+  const course = await Course.findById(id);
+  if (!course) {
+    console.log("course not found");
+    return;
+  }
+
+  course.isPublished = true;
+  course.author = "Another Author";
+  const result = await course.save();
+  console.log(result);
+}
+async function updateCourseUpdate(id) {
+  const result = await Course.updateOne(
+    { _id: id },
+    {
+      $set: {
+        author: "Late6002",
+        isPublished: false,
+      },
+    }
+  );
+
+  console.log(result);
+}
+
+async function updateCourseFindandUpdate(id) {
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: "Late6002 late",
+        isPublished: true,
+      },
+    },
+    { new: true }
+  );
+
+  console.log(course);
+}
+
+createCourse();
