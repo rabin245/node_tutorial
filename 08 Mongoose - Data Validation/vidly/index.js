8272d56606679c8687e59ddecbeca2549f9eedbc
rabin245 <drarabin@gmail.com>
2022-05-14 21:54:55 +0545

    adding mongodb to vidly genres api

---
+++ 08 Mongoose - Data Validation/vidly/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const express = require("express");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("connected to mongodb..."))
+  .catch((err) => console.error("could not connect to mongodb..."));
+
+app.use(express.json());
+
+app.use("/api/genres", genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
