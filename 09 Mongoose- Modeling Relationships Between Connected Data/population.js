4ef1b8c6951052bc9096addf731ef7dcf8809806
rabin245 <drarabin@gmail.com>
2022-05-15 14:21:19 +0545

    referencing documents

---
+++ 09 Mongoose- Modeling Relationships Between Connected Data/population.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const Author = mongoose.model(
+  "Author",
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String,
+  })
+);
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
+  })
+);
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const course = await Course.find().select("name");
+  console.log(course);
+}
+
+// createAuthor("Late", "My bio", "My website");
+
+createCourse("Node Course", "6280ba35ade38612f600e7ea");
+
+// listCourses()
