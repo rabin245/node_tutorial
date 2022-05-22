f2d93a69203e557b818688c1f7ea4ccab0afbae5
rabin245 <drarabin@gmail.com>
2022-05-22 14:09:35 +0545

    authentication and authorization intro

---
+++ 10 Authentication and Authorization/x.js
@@ -0,0 +1,10 @@
+// api/genres
+// api/movies
+// api/customers
+// api/rentals
+
+// Authentication -> identifying if user is who they claim to be (login)
+// Authorization -> if user has right permission to perform given operation
+
+// Register: POST /api/users
+// Login: POST /api/logins
