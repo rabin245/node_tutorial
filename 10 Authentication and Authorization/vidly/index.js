3e4b809ae89648e234bea283ec5cd6affdcb9f5c
rabin245 <drarabin@gmail.com>
2022-05-22 15:46:47 +0545

    authenticating users

--- 10 Authentication and Authorization/vidly/index.js
+++ 10 Authentication and Authorization/vidly/index.js
@@ -6,6 +6,7 @@ const genres = require("./routes/genres");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
@@ -21,6 +22,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
