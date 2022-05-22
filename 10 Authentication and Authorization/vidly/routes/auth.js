ad41d251f6af28c84cc83961296b32f948f7e0ba
rabin245 <drarabin@gmail.com>
2022-05-22 17:40:28 +0545

    storing secrets in environment variables

--- 10 Authentication and Authorization/vidly/routes/auth.js
+++ 10 Authentication and Authorization/vidly/routes/auth.js
@@ -1,6 +1,6 @@
+const config = require("config");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const _ = require("lodash");
 const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey")); //export vidly_jwtPrivateKey=mySecureKey
 
   res.send(token);
 });
