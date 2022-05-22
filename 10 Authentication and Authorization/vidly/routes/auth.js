83b00e5c1b897b6e7ca5b000b2b3724d818c99d6
rabin245 <drarabin@gmail.com>
2022-05-22 17:12:11 +0545

    generating authentication tokens

--- 10 Authentication and Authorization/vidly/routes/auth.js
+++ 10 Authentication and Authorization/vidly/routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const Joi = require("joi");
@@ -15,7 +16,9 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+
+  res.send(token);
 });
 
 function validate(req) {
