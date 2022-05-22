c3970280e38d2088a652b958be5cb9674eea33b4
rabin245 <drarabin@gmail.com>
2022-05-22 15:30:47 +0545

    hashing passwords

--- 10 Authentication and Authorization/vidly/routes/users.js
+++ 10 Authentication and Authorization/vidly/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
@@ -10,23 +11,13 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("Email already registered.");
 
-  //   user = new User({
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   });
-  // Note: for password complexity check joi-password-complexity package
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
-  // res.send(user); // we dont want to send back password in response
-  // first approach
-  //   res.send({
-  //     name: user.name,
-  //     email: user.email,
-  //   });
-  // second approach (using lodash)
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
