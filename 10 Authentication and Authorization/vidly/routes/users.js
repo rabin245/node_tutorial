39d9e94a291c9c2b2be71203d6fb2acdec27acdd
rabin245 <drarabin@gmail.com>
2022-05-22 15:16:59 +0545

    Using Lodash

--- 10 Authentication and Authorization/vidly/routes/users.js
+++ 10 Authentication and Authorization/vidly/routes/users.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
@@ -9,15 +10,24 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("Email already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  //   user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //   });
+  // Note: for password complexity check joi-password-complexity package
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   await user.save();
 
-  res.send(user);
+  // res.send(user); // we dont want to send back password in response
+  // first approach
+  //   res.send({
+  //     name: user.name,
+  //     email: user.email,
+  //   });
+  // second approach (using lodash)
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
