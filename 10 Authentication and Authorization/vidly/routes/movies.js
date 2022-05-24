8de06a3a4584274d2048e9bec010eeb2fb815bf0
rabin245 <drarabin@gmail.com>
2022-05-24 19:30:58 +0545

    protecting routes

--- 10 Authentication and Authorization/vidly/routes/movies.js
+++ 10 Authentication and Authorization/vidly/routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
   res.send(movies);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
