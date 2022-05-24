dd30c06beea9a43434b099a24bfd7584fd9e8a26
rabin245 <drarabin@gmail.com>
2022-05-24 20:00:19 +0545

    getting the current user

--- 10 Authentication and Authorization/vidly/routes/users.js
+++ 10 Authentication and Authorization/vidly/routes/users.js
@@ -1,8 +1,14 @@
+const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
