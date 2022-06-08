5a58e8a24de0a64f4f93e844c57154f2e177ca04
rabin245 <drarabin@gmail.com>
2022-06-08 20:07:41 +0545

    unhandled promise rejections

--- 11 Handling and Logging Errors/vidly/index.js
+++ 11 Handling and Logging Errors/vidly/index.js
@@ -13,23 +13,53 @@ const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const express = require("express");
+const { exceptions } = require("winston");
 const app = express();
 
-process.on("uncaughtException", (ex) => {
-  console.log("we got an uncaught exception");
+// process.on("uncaughtException", (ex) => {
+//   console.log("we got an uncaught exception");
+//   console.log(ex.message);
+//   winston.error(ex.message, ex);
+//   process.exit(1);
+// });
+
+winston.exceptions.handle(
+  new winston.transports.File({ filename: "uncaughExceptions.log" })
+);
+// winston.rejections.handle(
+//   new winston.transports.File({ filename: "unhandledRejections.log" })
+// );
+winston.add(
+  new winston.transports.File({ filename: "unhandledRejections.log" })
+);
+
+process.on("unhandledRejection", (ex) => {
+  console.log("we got an unhandled rejection");
+  console.log(ex.message);
   winston.error(ex.message, ex);
+  // process.exit(1);
 });
 
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
+winston.add(
+  new winston.transports.File({
+    filename: "logfile.log",
+    handleExceptions: false,
+    handleRejections: false,
+  })
+);
 winston.add(
   new winston.transports.MongoDB({
     db: "mongodb://localhost:27047/vidly", // in real world apps, seperate operational db and logging db
     options: { useNewUrlParser: true },
     level: "info",
+    handleExceptions: true,
+    handleRejections: true,
   })
 );
 
-throw new Error("Something failed during startup.");
+// throw new Error("Something failed during startup.");
+const p = Promise.reject(new Error("Something failed miserably!"));
+p.then(() => console.log("Done"));
 
 // export vidly_jwtPrivateKey=mySecureKey
 if (!config.get("jwtPrivateKey")) {
