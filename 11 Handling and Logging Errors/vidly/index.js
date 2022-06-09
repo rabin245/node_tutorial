4125c0f0ed8b32f0329ef4de3a1febb213d020d8
rabin245 <drarabin@gmail.com>
2022-06-09 18:16:05 +0545

    refactoring index.js- extracting routes

--- 11 Handling and Logging Errors/vidly/index.js
+++ 11 Handling and Logging Errors/vidly/index.js
@@ -1,20 +1,14 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const customers = require("./routes/customers");
-const genres = require("./routes/genres");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
-const { exceptions } = require("winston");
 const app = express();
+
+require("./startup/routes")(app);
 
 // process.on("uncaughtException", (ex) => {
 //   console.log("we got an uncaught exception");
@@ -74,16 +68,6 @@ mongoose
   })
   .then(() => console.log("connected to mongodb..."))
   .catch((err) => console.error("could not connect to mongodb..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
